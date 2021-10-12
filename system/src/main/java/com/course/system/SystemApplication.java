23ac4374027d69d6cd6e6cdb4fa8fd1aa3e8e0b2
ShunheWang <maisonwangsh@gmail.com>
2021-10-13 00:12:46 +0800

    1. 解决system无法注册到注册中心上，pom文件依赖错误

--- system/src/main/java/com/course/system/SystemApplication.java
+++ system/src/main/java/com/course/system/SystemApplication.java
@@ -4,9 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SystemApplication {
     private static final Logger log = LoggerFactory.getLogger(SystemApplication.class);
 
@@ -15,7 +17,7 @@ public class SystemApplication {
         SpringApplication sApp = new SpringApplication(SystemApplication.class);
         Environment env = sApp.run(args).getEnvironment();
         log.info("启动成功");
-        log.info("Eureka 地址：\t http://127.0.0.1:{}", env.getProperty("server.port"));
+        log.info("system 地址：\t http://127.0.0.1:{}", env.getProperty("server.port"));
 
     }
 }
