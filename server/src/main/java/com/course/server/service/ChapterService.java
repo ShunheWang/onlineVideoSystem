9c9eb7d06decc2a5fb03d25377fc68f0dc895d48
ShunheWang <maisonwangsh@gmail.com>
2021-10-28 16:11:32 +0800

    1. 增加CopyUtil工具类

--- server/src/main/java/com/course/server/service/ChapterService.java
+++ server/src/main/java/com/course/server/service/ChapterService.java
@@ -3,6 +3,7 @@ package com.course.server.service;
 import com.course.server.dto.Chapter;
 import com.course.server.dto.ChapterExample;
 import com.course.server.mapper.ChapterMapper;
+import com.course.server.util.CopyUtil;
 import com.course.server.util.UuidUtil;
 import com.course.server.vo.ChapterVO;
 import com.course.server.vo.PageVO;
@@ -27,20 +28,22 @@ public class ChapterService {
         List<Chapter> chapterDTOList = chapterMapper.selectByExample(chapterExample);
         PageInfo<Chapter> pageInfo = new PageInfo<>(chapterDTOList);
         pageVO.setTotal(pageInfo.getTotal());
-        List<ChapterVO> chapterVOList = new ArrayList<>();
-        for (int i = 0, l = chapterDTOList.size(); i < l; i++) {
-            Chapter chapter = chapterDTOList.get(i);
-            ChapterVO chapterVO = new ChapterVO();
-            BeanUtils.copyProperties(chapter, chapterVO);
-            chapterVOList.add(chapterVO);
-        }
-        pageVO.setList(chapterDTOList);
+//        List<ChapterVO> chapterVOList = new ArrayList<>();
+//        for (int i = 0, l = chapterDTOList.size(); i < l; i++) {
+//            Chapter chapter = chapterDTOList.get(i);
+//            ChapterVO chapterVO = new ChapterVO();
+//            BeanUtils.copyProperties(chapter, chapterVO);
+//            chapterVOList.add(chapterVO);
+//        }
+        List<ChapterVO> chapterVOList = CopyUtil.copyList(chapterDTOList, ChapterVO.class);
+        pageVO.setList(chapterVOList);
     }
 
     public void save(ChapterVO chapterVO) {
         chapterVO.setId(UuidUtil.getShortUuid());
-        Chapter chapter = new Chapter();
-        BeanUtils.copyProperties(chapterVO, chapter);
+//        Chapter chapter = new Chapter();
+//        BeanUtils.copyProperties(chapterVO, chapter);
+        Chapter chapter = CopyUtil.copy(chapterVO, Chapter.class);
         chapterMapper.insert(chapter);
     }
 
